263bf1f5293ec37eb52c90892972a95a7724a4ea
Jorge Israel Peña <jorge.israel.p@gmail.com>
2015-07-10 14:35:11 -0700

    add defer join test

--- test/test_run.rs
+++ test/test_run.rs
@@ -4,7 +4,7 @@ use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
 use std::thread;
-use eventual::{background, defer, Future, Async};
+use eventual::{background, defer, join, Future, Async};
 
 // TODO figure out how to get rid of unused import error here
 use syncbox::ThreadPool;
@@ -19,6 +19,30 @@ fn test_defer_runs_on_thread_pool() {
     });
     complete.complete(7);
     assert_eq!(Ok(7 + 5), res.await());
+}
+
+#[test]
+fn test_defer_can_be_joined() {
+    let pool = ThreadPool::single_thread();
+    let mut futures = vec![];
+    let mut completes = vec![];
+
+    for _ in (1 .. 6) {
+        let (complete, future) = Future::<i32, ()>::pair();
+        let res = defer(pool.clone(), future).and_then(|v: i32| {
+            Ok(v + 1)
+        });
+
+        futures.push(res);
+        completes.push(complete);
+    }
+
+    for (complete, num) in completes.into_iter().zip((1 .. 6)) {
+        complete.complete(num);
+    }
+
+    let res: Vec<i32> = join(futures).await().unwrap();
+    assert_eq!(res, vec![2, 3, 4, 5, 6]);
 }
 
 #[test]
